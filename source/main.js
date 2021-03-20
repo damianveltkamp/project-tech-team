fa8fff59bdc8b21c6c77bf8c2f75c7c21a6e0c77
Kip-Hub <daniel.kip@hva.nl>
2021-03-20 14:30:45 +0100

    test

--- source/main.js
+++ source/main.js
@@ -2,3 +2,4 @@ import moduleInit from '@javascript/utilities/module-init';
 import * as header from '@components/header/javascript/header';
 
 moduleInit('[js-hook-module-header]', 'header', () => header);
+// comment
