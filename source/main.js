4f82114f7c3d6f3ecb27811dd95ab01428814d36
damian1997 <d.veltkamp137@gmail.com>
2021-03-22 16:35:35 +0100

    Added form validation in frontend

--- source/main.js
+++ source/main.js
@@ -1,4 +1,6 @@
 import moduleInit from '@javascript/utilities/module-init';
 import * as header from '@components/header/javascript/header';
+import * as form from '@components/form/javascript/form';
 
 moduleInit('[js-hook-module-header]', 'header', () => header);
+moduleInit('[js-hook-module-form-validation]', '', () => form);
