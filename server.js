c53bc570ba0d4887d34a6da09b4e708699eae904
Kip-Hub <daniel.kip@hva.nl>
2021-03-28 16:51:37 +0200

    Some fixes and chat styling

--- server.js
+++ server.js
@@ -26,63 +26,64 @@ const server = http.createServer(app);
 const io = new socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log(`connected ${socket}`);
-  socket.on('joinRoom', (roomID) => {
-    console.log(roomID);
-    socket.join(`custom:${roomID}`);
-  });
+    console.log(`connected ${socket}`);
+    socket.on('joinRoom', (roomID) => {
+        console.log(roomID);
+        socket.join(`custom:${roomID}`);
+    });
 
-  socket.on('message', ({ message, roomID }) => {
-    socket.broadcast.to(`custom:${roomID}`).emit('send-message', message);
-  });
+    socket.on('message', ({ message, roomID }) => {
+        socket.broadcast.to(`custom:${roomID}`).emit('send-message', message);
+    });
 
-  socket.on('disconnect', () => {
-    socket.removeAllListeners();
-  });
+    socket.on('disconnect', () => {
+        socket.removeAllListeners();
+    });
 });
 
 mongoose.connect(dbUrl, {
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
 });
 
 const nunjucksEnv = nunjucks
-  .configure(['source/views', ...defaultHelpers.getComponentPaths()], {
-    autoescape: true,
-    express: app,
-  })
-  .addGlobal('jsBundle', defaultHelpers.getJsBundleName())
-  .addGlobal('cssBundle', defaultHelpers.getCssBundleName());
+    .configure(['source/views', ...defaultHelpers.getComponentPaths()], {
+        autoescape: true,
+        express: app,
+    })
+    .addGlobal('jsBundle', defaultHelpers.getJsBundleName())
+    .addGlobal('cssBundle', defaultHelpers.getCssBundleName());
 
 // TODO Favicon afvangen want word nu 2x heeeeeeye gelogt
 app
-  .use(compression())
-  .use(express.static('static'))
-  .use(cors())
-  .use(express.json())
-  .use(urlEncodedParser)
-  .use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      name: process.env.SESSION_NAME,
-      resave: false,
-      saveUninitialized: true,
-      store: new redisStore({ client: redisClient }),
-    }),
-  )
-  .use((req, res, next) => {
-    defaultHelpers.setCookieExpire(req);
-    if (req.session.userID) {
-      nunjucksEnv.addGlobal('userID', req.session.userID);
-      return next();
-    }
+    .use(compression())
+    .use(express.static('static'))
+    .use(cors())
+    .use(express.json())
+    .use(urlEncodedParser)
+    .use(
+        session({
+            secret: process.env.SESSION_SECRET,
+            name: process.env.SESSION_NAME,
+            resave: false,
+            saveUninitialized: true,
+            store: new redisStore({ client: redisClient }),
+        }),
+    )
+    .use((req, res, next) => {
+        defaultHelpers.setCookieExpire(req);
+        if (req.session.userID) {
+            nunjucksEnv.addGlobal('userID', req.session.userID);
+            nunjucksEnv.addGlobal('role', req.session.role);
+            return next();
+        }
 
-    nunjucksEnv.addGlobal('userID', false);
-    return next();
-  })
-  .set('view engine', 'html')
-  .set('io', io)
-  .use('/', router);
+        nunjucksEnv.addGlobal('userID', false);
+        return next();
+    })
+    .set('view engine', 'html')
+    .set('io', io)
+    .use('/', router);
 
 server.listen(port);
--- source/components/support/javascript/support.js
+++ source/components/support/javascript/support.js
@@ -1,61 +1,64 @@
 let socket = io();
 
 function createMesageElement(message, nodeClass) {
-  const node = document.createElement('div');
-  const textNode = document.createTextNode(message);
-  node.classList.add('support__message');
-  node.classList.add(nodeClass);
-  node.appendChild(textNode);
+    const node = document.createElement('div');
+    const textNode = document.createTextNode(message);
+    node.classList.add('support__message');
+    node.classList.add(nodeClass);
+    node.appendChild(textNode);
 
-  return node;
+    return node;
 }
 
 export function requestSupport() {
-  const supportButton = document.querySelector('[js-hook-module-support]');
-  supportButton.addEventListener('click', () => {
-    socket.emit('joinRoom', event.target.dataset.id);
-  });
+    const supportButton = document.querySelector('[js-hook-module-support]');
+    supportButton.addEventListener('click', () => {
+        socket.emit('joinRoom', event.target.dataset.id);
+    });
 }
 
 export function openChat() {
-  const openChatButton = document.querySelectorAll('[js-hook-module-chat]');
-  const chatContainer = document.querySelector('[js-hook-support-chat]');
+    const openChatButton = document.querySelectorAll('[js-hook-module-chat]');
+    const chatContainer = document.querySelector('[js-hook-support-chat]');
 
-  [...openChatButton].forEach((node) => {
-    node.addEventListener('click', () => {
-      chatContainer.classList.remove('hide');
-      const roomidContainer = chatContainer.querySelector('[type=hidden]');
-      roomidContainer.value = node.dataset.id;
-      socket.emit('joinRoom', node.dataset.id);
+    [...openChatButton].forEach((node) => {
+        node.addEventListener('click', () => {
+            chatContainer.classList.remove('hide');
+            const roomidContainer = chatContainer.querySelector('[type=hidden]');
+            roomidContainer.value = node.dataset.id;
+            socket.emit('joinRoom', node.dataset.id);
+        });
     });
-  });
 }
 
 export function sendChatMessage() {
-  const chatForm = document.querySelector('[js-hook-module-chat-form]');
-  chatForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const roomID = document.querySelector('[js-hook-roomid]').value;
-    const message = document.querySelector('[js-hook-chat-message]').value;
-    const messageContainer = document.querySelector(
-      '[js-hook-module-get-messages]',
-    );
+    const chatForm = document.querySelector('[js-hook-module-chat-form]');
+    chatForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const roomID = document.querySelector('[js-hook-roomid]').value;
+        const date = new Date();
+        const timeString = date.getHours() + ":" + date.getMinutes() + " ";
 
-    socket.emit('message', { message, roomID });
-    const messageElement = createMesageElement(message, 'sender');
-    messageContainer.insertBefore(messageElement, messageContainer.firstChild);
-  });
+
+        const message = timeString + document.querySelector('[js-hook-chat-message]').value;
+        const messageContainer = document.querySelector(
+            '[js-hook-module-get-messages]',
+        );
+
+        socket.emit('message', { message, roomID });
+        const messageElement = createMesageElement(message, 'sender');
+        messageContainer.insertBefore(messageElement, messageContainer.firstChild);
+    });
 }
 
 export function getChatMessages() {
-  // TODO append html to ontvnager
-  socket.on('send-message', (message) => {
-    const messageContainer = document.querySelector(
-      '[js-hook-module-get-messages]',
-    );
+    // TODO append html to ontvnager
+    socket.on('send-message', (message) => {
+        const messageContainer = document.querySelector(
+            '[js-hook-module-get-messages]',
+        );
 
-    const messageElement = createMesageElement(message, 'receiver');
-    messageContainer.insertBefore(messageElement, messageContainer.firstChild);
-    console.log(message);
-  });
+        const messageElement = createMesageElement(message, 'receiver');
+        messageContainer.insertBefore(messageElement, messageContainer.firstChild);
+    });
 }
